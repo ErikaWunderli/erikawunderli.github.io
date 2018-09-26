31356765dfd9ead54bbf41e58a3e8b9c832a7082
Erika Wunderli-Vallai <vallai.erika@gmail.com>
2018-09-26 23:34:12 +0200

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); 
+    
+ $(document).ready(function(){
+    $('.slider').slider();
+  });
+    
+    // end of document ready
+})(jQuery); // end of jQuery name space
