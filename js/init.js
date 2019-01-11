acc280e210b83ebff1a621430bd9bd18645b30c1
Erika Wunderli-Vallai <vallai.erika@gmail.com>
2019-01-11 22:52:37 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -9,9 +9,6 @@
  $(document).ready(function(){
     $('.slider').slider();
   });
- $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
     
     // end of document ready
 })(jQuery); // end of jQuery name space
